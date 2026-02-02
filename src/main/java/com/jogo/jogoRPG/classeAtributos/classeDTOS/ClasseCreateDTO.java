7b757805086bf9ebc99624c221b5e21bcdd3a9b5
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2026-02-02 20:38:29 -0300

    configurando o DTO Do Create Da Classe

--- src/main/java/com/jogo/jogoRPG/classeAtributos/classeDTOS/ClasseCreateDTO.java
+++ src/main/java/com/jogo/jogoRPG/classeAtributos/classeDTOS/ClasseCreateDTO.java
@@ -1,4 +1,19 @@
 package com.jogo.jogoRPG.classeAtributos.classeDTOS;
 
-public record ClasseCreateDTO() {
-}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record ClasseCreateDTO(
+
+        @NotBlank(message = "Nome é obrigatório")
+        @Size(min = 6, message = "Nome deve ter no mínimo 6 caracteres")
+        String nome,
+
+        @NotBlank(message = "Descrição é obrigatória")
+        @Size(min = 10, message = "Descrição deve ter no mínimo 10 caracteres")
+        String descricao,
+
+        @NotBlank(message = "Rank é obrigatório")
+        @Size(min = 1, message = "Rank deve ter no mínimo 1 caractere")
+        String rank
+) {}
