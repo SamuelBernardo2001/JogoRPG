853cab8e7bcff94418c9ea7dda18a4c07768aa88
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2026-02-02 18:19:21 -0300

    iniciando projeto

---
+++ src/main/java/com/jogo/jogoRPG/JogoRpgApplication.java
@@ -0,0 +1,13 @@
+package com.jogo.jogoRPG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogoRpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JogoRpgApplication.class, args);
+	}
+
+}
