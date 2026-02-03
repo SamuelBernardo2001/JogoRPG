628eefe0ec28a3908d9e9ff63021c7de462564a8
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2026-02-02 21:04:20 -0300

    adicionando as configurações do Lombok no Model para recebe os Gets e Sets e Construtores

--- src/main/java/com/jogo/jogoRPG/classeAtributos/classeModels/ClasseModel.java
+++ src/main/java/com/jogo/jogoRPG/classeAtributos/classeModels/ClasseModel.java
@@ -3,9 +3,17 @@ package com.jogo.jogoRPG.classeAtributos.classeModels;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "classes")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class ClasseModel {
 
     @Id
