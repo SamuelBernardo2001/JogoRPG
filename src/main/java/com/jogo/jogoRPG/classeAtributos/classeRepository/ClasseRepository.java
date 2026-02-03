88828d2d06bc5a8be1db7bc18719d49a0563be15
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2026-02-02 22:23:13 -0300

    criando o repository para a integração com o banco de dados

---
+++ src/main/java/com/jogo/jogoRPG/classeAtributos/classeRepository/ClasseRepository.java
@@ -0,0 +1,8 @@
+package com.jogo.jogoRPG.classeAtributos.classeRepository;
+
+import com.jogo.jogoRPG.classeAtributos.classeModels.ClasseModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClasseRepository extends JpaRepository<ClasseModel, Long> {
+
+}
