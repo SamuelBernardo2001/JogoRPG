c738bbad5ef1a555011b65001a067028bca9aa01
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2026-02-02 22:40:02 -0300

    A classe agora recebe Repository, Service e Mapper via construtor, evitando dependências implícitas e melhorando a legibilidade, imutabilidade e testabilidade do código.

--- src/main/java/com/jogo/jogoRPG/classeAtributos/classeUseCase/CreateClasseUseCase.java
+++ src/main/java/com/jogo/jogoRPG/classeAtributos/classeUseCase/CreateClasseUseCase.java
@@ -1,7 +1,22 @@
 package com.jogo.jogoRPG.classeAtributos.classeUseCase;
 
+import com.jogo.jogoRPG.classeAtributos.classeRepository.ClasseRepository;
+import com.jogo.jogoRPG.classeAtributos.classeService.ClasseServiceCreate;
+import com.jogo.jogoRPG.classeAtributos.mapper.ClasseMapper;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CreateClasseUseCase {
+
+    private final ClasseRepository repository;
+    private final ClasseServiceCreate serviceCreate;
+    private final ClasseMapper mapper;
+
+    public CreateClasseUseCase(ClasseRepository repository, ClasseServiceCreate serviceCreate, ClasseMapper mapper) {
+        this.repository = repository;
+        this.serviceCreate = serviceCreate;
+        this.mapper = mapper;
+    }
+
+
 }
