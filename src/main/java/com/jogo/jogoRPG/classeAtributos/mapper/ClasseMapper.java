e3b3ef17e2ccf37940cc6b634f241149fcc8513b
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2026-02-02 21:05:19 -0300

    Criando O Metodo para Converte O DTOCREATE para Entity

--- src/main/java/com/jogo/jogoRPG/classeAtributos/mapper/ClasseMapper.java
+++ src/main/java/com/jogo/jogoRPG/classeAtributos/mapper/ClasseMapper.java
@@ -1,4 +1,16 @@
 package com.jogo.jogoRPG.classeAtributos.mapper;
 
+import com.jogo.jogoRPG.classeAtributos.classeDTOS.ClasseCreateDTO;
+import com.jogo.jogoRPG.classeAtributos.classeModels.ClasseModel;
+
 public class ClasseMapper {
+
+    // Criando O metodo de converte DTO para Entity do CreateDTO
+    public static ClasseModel toEntity(ClasseCreateDTO create){
+        ClasseModel classeModel = new ClasseModel();
+        classeModel.setNome(create.nome());
+        classeModel.setDescricao(create.descricao());
+        classeModel.setRank(create.rank());
+        return classeModel;
+    }
 }
