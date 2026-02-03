061a0d08b1aa70c4cff3b16de671396cd61355b8
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2026-02-02 22:04:32 -0300

    verificando o tamanho do nome para valida o nome

--- src/main/java/com/jogo/jogoRPG/classeAtributos/classeService/ClasseServiceCreate.java
+++ src/main/java/com/jogo/jogoRPG/classeAtributos/classeService/ClasseServiceCreate.java
@@ -14,6 +14,8 @@ public class ClasseServiceCreate {
         if (classe.getNome() == null || classe.getNome().trim().isEmpty()) {
             throw new IllegalArgumentException("O nome da classe não pode ser nulo ou vazio.");}
 
+        if (classe.getNome().length() < 5 || classe.getNome().length() > 15) {
+            throw new IllegalArgumentException("O nome da classe deve ter entre 5 e 15 caracteres.");}
     }
 
 }
