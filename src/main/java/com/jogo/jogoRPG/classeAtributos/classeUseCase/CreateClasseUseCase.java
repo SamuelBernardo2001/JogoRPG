5604d131c19ec58a169866197122166cd1aee0ab
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2026-02-02 22:56:00 -0300

    CreateClasseUseCase
    - Converte ClasseCreateDTO para ClasseModel via mapper
    - Aplica regras de negócio no ClasseServiceCreate
    - Persiste entidade via ClasseRepository
    - Retorna ClasseResponseDTO

--- src/main/java/com/jogo/jogoRPG/classeAtributos/classeUseCase/CreateClasseUseCase.java
+++ src/main/java/com/jogo/jogoRPG/classeAtributos/classeUseCase/CreateClasseUseCase.java
@@ -1,5 +1,8 @@
 package com.jogo.jogoRPG.classeAtributos.classeUseCase;
 
+import com.jogo.jogoRPG.classeAtributos.classeDTOS.ClasseCreateDTO;
+import com.jogo.jogoRPG.classeAtributos.classeDTOS.ClasseResponseDTO;
+import com.jogo.jogoRPG.classeAtributos.classeModels.ClasseModel;
 import com.jogo.jogoRPG.classeAtributos.classeRepository.ClasseRepository;
 import com.jogo.jogoRPG.classeAtributos.classeService.ClasseServiceCreate;
 import com.jogo.jogoRPG.classeAtributos.mapper.ClasseMapper;
@@ -10,13 +13,20 @@ public class CreateClasseUseCase {
 
     private final ClasseRepository repository;
     private final ClasseServiceCreate serviceCreate;
-    private final ClasseMapper mapper;
 
-    public CreateClasseUseCase(ClasseRepository repository, ClasseServiceCreate serviceCreate, ClasseMapper mapper) {
+    public CreateClasseUseCase(ClasseRepository repository, ClasseServiceCreate serviceCreate) {
         this.repository = repository;
         this.serviceCreate = serviceCreate;
-        this.mapper = mapper;
     }
 
+    public ClasseResponseDTO execute(ClasseCreateDTO dto) {
+        ClasseModel classeModel = ClasseMapper.toEntity(dto);
+
+        serviceCreate.valida(classeModel);
+
+        ClasseModel salva = repository.save(classeModel);
+
+        return ClasseMapper.toDTO(salva);
+    }
 
 }
