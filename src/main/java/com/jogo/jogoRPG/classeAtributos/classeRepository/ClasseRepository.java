3fc226bdde4de6e0f1772266804e3b0976888d47
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2026-02-02 22:29:25 -0300

    criando um metodo para busca o nome

--- src/main/java/com/jogo/jogoRPG/classeAtributos/classeRepository/ClasseRepository.java
+++ src/main/java/com/jogo/jogoRPG/classeAtributos/classeRepository/ClasseRepository.java
@@ -3,7 +3,10 @@ package com.jogo.jogoRPG.classeAtributos.classeRepository;
 import com.jogo.jogoRPG.classeAtributos.classeModels.ClasseModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClasseRepository extends JpaRepository<ClasseModel, Long> {
 
     boolean existsByNome(String nome);
+    Optional<ClasseModel> findByNome(String nome);
 }
